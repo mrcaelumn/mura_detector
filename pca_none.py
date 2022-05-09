cb7732b8e6b2857467f648e820d7d2842f20ff5a
mrcaelumn <caelumnproperty@gmail.com>
2022-05-09 23:52:20 +0800

    add PCA grayscale

--- pca_none.py
+++ pca_none.py
@@ -1,6 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 from glob import glob
 import random
 import math
+import os
 import numpy as np
 from PIL import Image
 import time
@@ -8,93 +15,141 @@ from random import sample
 from sklearn.metrics import roc_auc_score
 from sklearn.decomposition import PCA
 import cv2
+import tensorflow as tf
 from sklearn.metrics import recall_score,accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import confusion_matrix
 
 
-
-sob_normal_train_img_fn = glob('./mura_data/RGB/20220407/data/train_data/normal/*.png')
-sob_mura_train_img_fn = glob('./mura_data/RGB/20220407/data/train_data/defect/*.png')#
-sob_normal_test_img_fn = glob('./mura_data/RGB/20220407/data/test_data/normal/*.png')#8261
-sob_mura_img_fn = glob('./mura_data/RGB/20220407/data/test_data/defect/*.png')#305
-print(len(sob_normal_train_img_fn))
-print(len(sob_mura_train_img_fn))
-print(len(sob_normal_test_img_fn))
-print(len(sob_mura_img_fn))
+# In[ ]:
 
 
-random.seed(time.time())
+sob_normal_train_img_fn = glob('mura_data/RGB/mura_march_clean/train_data/normal/*.png')
 
-random.shuffle(sob_normal_test_img_fn)
-sob_normal_test_img_fn = sob_normal_test_img_fn[:1000]
-random.shuffle(sob_mura_train_img_fn)
-sob_mura_train_img_fn = sob_mura_train_img_fn[:695]
-sob_mura_img_fn.extend(sob_mura_train_img_fn)
 
-print("none")
-print(len(sob_normal_train_img_fn))
-print(len(sob_normal_test_img_fn))
-print(len(sob_mura_img_fn))
+# In[ ]:
 
-N,w=10000,128
-read_img = lambda fn: np.array(Image.open(fn).resize((w,w)).convert('L')).ravel()
+
+ORI_SIZE = (271, 481)
+
+def sliding_window(image, stepSize=20, windowSize=(256, 256)):
+    current_std = 0
+    current_image = None
+    # print(image.shape)
+    y_end_crop, x_end_crop = False, False
+    
+    for y in range(0, image.shape[0], stepSize):
+        y_end_crop = False
+        
+        for x in range(0, image.shape[1], stepSize):
+            
+            x_end_crop = False
+            
+            crop_y = y
+            if (y + windowSize[0]) > ORI_SIZE[0]:
+                crop_y =  ORI_SIZE[0] - windowSize[0]
+                y_end_crop = True
+            
+            crop_x = x
+            if (x + windowSize[1]) > ORI_SIZE[1]:
+                crop_x = ORI_SIZE[1] - windowSize[1]
+                x_end_crop = True
+            
+            # print(x, y)
+            img = image[crop_y:y + windowSize[0], crop_x:x + windowSize[1]]
+            std_image = np.std(img)
+            # print(std_image)
+            if current_std == 0 or std_image < current_std :
+                current_std = std_image
+                current_image = img
+            
+            if x_end_crop:
+                break
+                
+        if x_end_crop and y_end_crop:
+            break
+            
+    return current_image   
+
+
+# In[ ]:
+
+
+N,w=300,128
+
+def open_image(fn):
+    img = cv2.imread(fn)
+    
+    img = sliding_window(img)
+    # print(img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, dsize=(w, w), interpolation=cv2.INTER_CUBIC)
+    return img
+    
+read_img = lambda fn: np.array(open_image(fn)).ravel()
 load_imgs = lambda fn_list: np.array([read_img(fn) for fn in fn_list]).astype("float32")
 
+
+# In[ ]:
+
+
 sob_normal_train_imgs = load_imgs(sample(sob_normal_train_img_fn,N))
-sob_normal_test_imgs = load_imgs(sob_normal_test_img_fn)
-sob_mura_imgs = load_imgs(sample(sob_mura_img_fn,1000))
 
 print(sob_normal_train_imgs.shape)
 
 
+# In[ ]:
 
 
-#pca = PCA(n_components=512)
 pca = PCA(n_components=256)
 sob_normal_train_PCA = pca.fit(sob_normal_train_imgs)
 print(sob_normal_train_PCA)
-def compute_dist(x,pca):
-    x_pca = pca.transform(x)
-    x_inv = pca.inverse_transform(x_pca)
-
-    return np.linalg.norm(x_inv-x,axis=1)
-
-self_dist = compute_dist(sob_normal_train_imgs, pca)
-print("self_dist.shape:",self_dist.shape)
-print("normal_train_imgs:",self_dist.mean(),self_dist.std())
-
-ndist = compute_dist(sob_normal_test_imgs, pca)
-print("ndist.shape:",ndist.shape)
-print("normal_test_imgs",ndist.mean(),ndist.std())#8261
-
-mdist = compute_dist(sob_mura_imgs, pca)
-print("mdist.shape",mdist.shape)
-print("mura_imgs",mdist.mean(),mdist.std())#1000
-
-dists = np.concatenate([ndist,mdist])
-print(len(dists))
 
 
-pred = []
-for i in dists:
-    if i>= (self_dist.mean()+self_dist.std()):
-        pred.append(1)
-    else:
-        pred.append(0)
-label = [0]*ndist.shape[0]+[1]*mdist.shape[0]
-
-print(confusion_matrix(label, pred))
-print("recall:",recall_score(label,pred))
-print("precision:",precision_score(label,pred))
-
-print("acc",accuracy_score(label,pred))
+# In[ ]:
 
 
+def PCA_IMG(inputFile, outputFile, inner_pca):
+    img = np.array(open_image(inputFile)).ravel().astype("float32")
+    x_pca = inner_pca.transform([img])
+    x_inv = inner_pca.inverse_transform(x_pca)
 
-print("auc:",roc_auc_score([0]*ndist.shape[0]+[1]*mdist.shape[0],dists))
+    img = np.reshape(x_inv[0], (-1, w))
+
+    # print(img.shape)
+    # cv2.imwrite(OutputFile, img)
+    cv2.imwrite("test.png", img)    
 
 
+# In[ ]:
+
+
+## convert colour of images
+for mode in ["test_data","train_data"]:
+    for class_name in ["normal", "defect"]:
+        Input_dir = f'mura_data/RGB/mura_march_clean/{mode}/{class_name}/'
+        Out_dir = f'mura_data/RGB/mura_pca_clean/{mode}/{class_name}/'
+        a = os.listdir(Input_dir)
+        index = 0
+        for i in a:
+            index += 1
+            if i != ".DS_Store" and i != ".ipynb_checkpoints":
+
+                inputFile = Input_dir+i
+                OutputFile = Out_dir+i
+                PCA_IMG(inputFile, OutputFile, pca)
+                
+                # if index == 1:
+                #     break
+            if index % 1000 == 0:
+                print("file: ",index)
+        print("done.", class_name, mode)
+        # if index == 1:
+        #     break
+    #     break
+
+
+# In[ ]:
 
 
 
