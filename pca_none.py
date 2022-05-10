d85a15c5abe0e476ae1191e4fc74df1397e42eb8
mrcaelumn <caelumnproperty@gmail.com>
2022-05-10 11:53:57 +0800

    add PCA RGB

--- pca_none.py
+++ pca_none.py
@@ -30,8 +30,6 @@ sob_normal_train_img_fn = glob('mura_data/RGB/mura_march_clean/train_data/normal
 # In[ ]:
 
 
-ORI_SIZE = (271, 481)
-
 def sliding_window(image, stepSize=20, windowSize=(256, 256)):
     current_std = 0
     current_image = None
@@ -46,12 +44,12 @@ def sliding_window(image, stepSize=20, windowSize=(256, 256)):
             x_end_crop = False
             
             crop_y = y
-            if (y + windowSize[0]) > ORI_SIZE[0]:
+            if (y + windowSize[0]) > image.shape[0]:
                 crop_y =  ORI_SIZE[0] - windowSize[0]
                 y_end_crop = True
             
             crop_x = x
-            if (x + windowSize[1]) > ORI_SIZE[1]:
+            if (x + windowSize[1]) > image.shape[1]:
                 crop_x = ORI_SIZE[1] - windowSize[1]
                 x_end_crop = True
             
@@ -75,50 +73,107 @@ def sliding_window(image, stepSize=20, windowSize=(256, 256)):
 # In[ ]:
 
 
-N,w=300,128
+N,w=256,256
 
 def open_image(fn):
     img = cv2.imread(fn)
-    
+    # print(img.shape)
     img = sliding_window(img)
     # print(img)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.resize(img, dsize=(w, w), interpolation=cv2.INTER_CUBIC)
-    return img
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-read_img = lambda fn: np.array(open_image(fn)).ravel()
-load_imgs = lambda fn_list: np.array([read_img(fn) for fn in fn_list]).astype("float32")
+    img = cv2.resize(img, dsize=(w, w), interpolation=cv2.INTER_CUBIC)
+    b, g, r = cv2.split(img)
+    
+    flat_b = np.array(b).ravel()
+    flat_g = np.array(g).ravel()
+    flat_r = np.array(r).ravel()
+    return flat_b, flat_g, flat_r
+
+def load_imgs(img_list):
+    b_array = []
+    g_array = []
+    r_array = []
+    
+    for fn in img_list:
+        flat_b, flat_g, flat_r = open_image(fn)
+        b_array.append( np.array(flat_b).astype("float32") )
+        g_array.append( np.array(flat_g).astype("float32") )
+        r_array.append( np.array(flat_r).astype("float32") )
+        
+    
+    return np.array(b_array), np.array(g_array), np.array(r_array)
+    
+    
+# read_img = lambda fn: np.array(open_image(fn)).ravel()
+# load_imgs = lambda fn_list: np.array([read_img(fn) for fn in fn_list]).astype("float32")
 
 
 # In[ ]:
 
 
-sob_normal_train_imgs = load_imgs(sample(sob_normal_train_img_fn,N))
+b_normal_train_imgs, g_normal_train_imgs, r_normal_train_imgs = load_imgs(sample(sob_normal_train_img_fn,N))
 
-print(sob_normal_train_imgs.shape)
+print(b_normal_train_imgs.shape)
+print(g_normal_train_imgs.shape)
+print(r_normal_train_imgs.shape)
 
 
 # In[ ]:
 
 
 pca = PCA(n_components=256)
-sob_normal_train_PCA = pca.fit(sob_normal_train_imgs)
-print(sob_normal_train_PCA)
+b_normal_train_PCA = pca.fit(b_normal_train_imgs)
+g_normal_train_PCA = pca.fit(g_normal_train_imgs)
+r_normal_train_PCA = pca.fit(r_normal_train_imgs)
+print(b_normal_train_PCA)
+print(g_normal_train_PCA)
+print(r_normal_train_PCA)
 
 
 # In[ ]:
 
 
-def PCA_IMG(inputFile, outputFile, inner_pca):
-    img = np.array(open_image(inputFile)).ravel().astype("float32")
-    x_pca = inner_pca.transform([img])
-    x_inv = inner_pca.inverse_transform(x_pca)
+def PCA_IMG(inputFile, outputFile, b_inner_pca, g_inner_pca, r_inner_pca):
+    b, g, r = open_image(inputFile)
+    # r_scaled = r / 255
+    r_scaled = [r] 
+    
+    # g_scaled = g / 255
+    g_scaled = [g] 
+    
+    # b_scaled = b / 255
+    b_scaled = [b] 
+    
 
-    img = np.reshape(x_inv[0], (-1, w))
+    pca_r_trans = r_inner_pca.transform(r_scaled)
 
-    # print(img.shape)
-    # cv2.imwrite(OutputFile, img)
-    cv2.imwrite("test.png", img)    
+    pca_g_trans = g_inner_pca.transform(g_scaled)
+
+    pca_b_trans = b_inner_pca.transform(b_scaled)
+
+    pca_r_org = r_inner_pca.inverse_transform(pca_r_trans)
+    pca_r_final = np.reshape(pca_r_org[0], (-1, w))
+    
+    # print(pca_r_final.shape)
+    
+    pca_g_org = g_inner_pca.inverse_transform(pca_g_trans)
+    pca_g_final = np.reshape(pca_g_org[0], (-1, w))
+    # print(pca_g_final.shape)
+    
+    pca_b_org = b_inner_pca.inverse_transform(pca_b_trans)
+    pca_b_final = np.reshape(pca_b_org[0], (-1, w))
+    # print(pca_b_final.shape)
+    
+    
+    img_compressed = cv2.merge((pca_b_final, pca_g_final, pca_r_final))
+    # print(img_compressed.shape)
+    # print(img_compressed)
+    # img = img_compressed * 255
+    img = img_compressed
+    cv2.imwrite(outputFile, img)
+    # cv2.imwrite("test.png", img)
+    
 
 
 # In[ ]:
@@ -137,7 +192,7 @@ for mode in ["test_data","train_data"]:
 
                 inputFile = Input_dir+i
                 OutputFile = Out_dir+i
-                PCA_IMG(inputFile, OutputFile, pca)
+                PCA_IMG(inputFile, OutputFile, b_normal_train_PCA, g_normal_train_PCA, r_normal_train_PCA)
                 
                 # if index == 1:
                 #     break
@@ -147,10 +202,4 @@ for mode in ["test_data","train_data"]:
         # if index == 1:
         #     break
     #     break
-
-
-# In[ ]:
-
-
-
 
